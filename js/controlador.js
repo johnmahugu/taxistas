9dd3e3d152c6fc8bd8f9c5a12e6f7fcdfa0251c5
Doriel Fonseca <doriel3572@gmail.com>
2014-06-15 09:10:35 -0700

    Only need on js file

--- js/controlador.js
+++ js/controlador.js
@@ -1,9 +1,1 @@
-var myApp = angular.module('myApp', []);
-
-myApp.controller('meuControlador', ['$scope', '$http', function($scope, $http) {
-  $http.get('js/lib/dados_taxi.json').success(function(data) {
-    $scope.taxistas = data;
-    $scope.taxistasOrder = 'nome';
-  });
-}]);
 
