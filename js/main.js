f2c47cea7c3b3d7dfda0e879f6279c4b87fe428f
Doriel Fonseca <doriel3572@gmail.com>
2014-06-22 11:06:39 -0700

    js for footer imgs

--- js/main.js
+++ js/main.js
@@ -6,3 +6,24 @@ myApp.controller('meuControlador', ['$scope', '$http', function($scope, $http) {
     $scope.taxistasOrder = 'nome';
   });
 }]);
+
+//img 
+function facehover() {
+  document.querySelector('#minha_imagem').src = "img/facebook_hover.png";
+}
+function faceout() {
+  document.querySelector('#minha_imagem').src = "img/facebook_norm.png";
+}
+function instahover() {
+  document.querySelector('#minha_imagem_pri').src = "img/instagram_hover.png";
+}
+function instaout() {
+  document.querySelector('#minha_imagem_pri').src = "img/instagram_norm.png";
+}
+function twithover() {
+  document.querySelector('#minha_imagem_sec').src = "img/twitter_hover.png";
+}
+function twitout() {
+  document.querySelector('#minha_imagem_sec').src = "img/twitter_norm.png";
+}
+//over   
